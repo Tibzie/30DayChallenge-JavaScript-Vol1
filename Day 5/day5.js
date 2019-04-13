d0113da1b2a2272fc7a0589f0c1ab8fc199087fc
Tibor Gazsovics <gt008@freemail.hu>
2019-04-13 16:53:04 +0100

    day 5

---
+++ Day 5/day5.js
@@ -0,0 +1,12 @@
+
+// Create a function that returns true if a string contains any spaces.
+// Try doing this without RegEx.
+
+
+function hasSpaces(str) {
+    return str.includes(" ") ? true : false;
+}
+
+console.log(hasSpaces("Do u kno da wae?"));
+
+
