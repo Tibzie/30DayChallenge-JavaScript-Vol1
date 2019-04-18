b548052e3a342ef71310643051ff1767520ea8a9
Tibor Gazsovics <gt008@freemail.hu>
2019-04-18 11:06:51 +0100

    day 10

---
+++ Day 10/day10.js
@@ -0,0 +1,13 @@
+
+// Create a function to remove all null values from an array.
+// Bonus cookie points:
+// - remove all undefined values from the array
+// - remove all NaN values from the array
+
+function removeNull(arr) {
+	return arr.filter((myArr) => {
+        return myArr !== null && typeof(myArr) === 'string' || typeof(myArr) === 'number' && !isNaN(myArr) && myArr !== undefined
+    });
+}
+
+console.log(removeNull(['spray', 'limit', null, 1, NaN, undefined, 'elite', 'exuberant', 'destruction', 'present']));
