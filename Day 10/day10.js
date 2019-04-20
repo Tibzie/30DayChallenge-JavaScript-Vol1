5e69d9c9c511376e0379d03c972f8ad583ba59a2
Tibor Gazsovics <gt008@freemail.hu>
2019-04-20 20:32:19 +0100

    day 11

--- Day 10/day10.js
+++ Day 10/day10.js
@@ -9,5 +9,4 @@ function removeNull(arr) {
         return myArr !== null && typeof(myArr) === 'string' || typeof(myArr) === 'number' && !isNaN(myArr) && myArr !== undefined
     });
 }
-
 console.log(removeNull(['spray', 'limit', null, 1, NaN, undefined, 'elite', 'exuberant', 'destruction', 'present']));
---
+++ Day 11/day11.js
@@ -0,0 +1,9 @@
+
+// Create a function that returns true if an integer is divisible by 5, and false otherwise.
+
+function divisibleByFive(n) {
+    return n%5 == 0 ? true: false;
+}
+
+
+console.log(divisibleByFive(0));
