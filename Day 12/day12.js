bd91b2cd8fd5648d1097a1aec4fd6701c04e1469
Tibor Gazsovics <gt008@freemail.hu>
2019-04-21 22:54:03 +0100

    day 12

---
+++ Day 12/day12.js
@@ -0,0 +1,9 @@
+
+// Create a function that returns true if a string is empty and false otherwise.
+
+
+function isEmpty(s) {
+    return s.length === 0 ? true : false;
+}
+
+console.log(isEmpty(" hi"));
