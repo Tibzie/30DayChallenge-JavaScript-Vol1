4a55fedfc27d8aac94223b05fba5d106d7cd828e
Tibor Gazsovics <gt008@freemail.hu>
2019-04-27 15:39:23 +0100

    day 16

---
+++ Day 16/day16.js
@@ -0,0 +1,8 @@
+// Create a function that returns the smaller number.
+// Numbers will be represented as strings, and your output should also be a string.
+
+function smallerNum(n1, n2) {
+    return Math.min(n1, n2).toString();
+}
+
+console.log(smallerNum("7", "-6"));
