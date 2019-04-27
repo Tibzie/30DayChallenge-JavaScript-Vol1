b88d8ad5d31423f3801ce44be419618b46b481c7
Tibor Gazsovics <gt008@freemail.hu>
2019-04-27 15:50:37 +0100

    extra 2

---
+++ Day 16/extra2.js
@@ -0,0 +1,9 @@
+// Create a function that takes in a word and determines whether or not it is plural.
+// A plural word is one that ends in "s".
+
+
+function isPlural(word) {
+	return word.endsWith("s");
+}
+
+console.log(isPlural("changes"));
