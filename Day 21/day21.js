7d9fec806b648803042c68199e0132185447e051
Tibor Gazsovics <gt008@freemail.hu>
2019-06-02 10:17:14 +0100

    day 21

---
+++ Day 21/day21.js
@@ -0,0 +1,12 @@
+// Your friend is trying to write a function that removes all vowels from a string. They write:
+//function removeVowels(str) {
+    //return str.replace(/[aeiou]/, "")
+//}
+
+// However, it seems that it doesn't work? Fix your friend's code so that it actually does remove all vowels.
+
+function removeVowels(str) {
+    return str.replace(/[aeiou]/gi, '')
+}
+
+console.log(removeVowels("apple"));
