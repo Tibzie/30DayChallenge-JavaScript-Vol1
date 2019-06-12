642e8c353c758951a8bf48448617938d9063530f
Tibz <38702209+Tibzie@users.noreply.github.com>
2019-06-12 03:50:49 +0100

    changed it a bit

--- Day 23/day23.js
+++ Day 23/day23.js
@@ -1,3 +1,5 @@
 // Create a function that takes a name and returns a greeting. Don't use a normal function but use an "arrow function".
 
-const helloName = name => `Hello ${name}!`
+const helloName = name => `Hello ${name}!`;
+
+console.log(helloName('Tibor!'));
